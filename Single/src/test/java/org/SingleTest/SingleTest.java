785adb14219afed272f2e9cda34cbfc0d691b005
rnvignesh2 <rnvignesh2@gmail.com>
2022-11-18 20:00:33 +0530

    first commit

---
+++ Single/src/test/java/org/SingleTest/SingleTest.java
@@ -0,0 +1,14 @@
+package org.SingleTest;
+
+public class SingleTest {
+
+	private void students() {
+System.out.println("name is vignesh");
+	}
+
+	public static void main(String[] args) {
+		
+		new SingleTest().students();
+	}
+	
+}
